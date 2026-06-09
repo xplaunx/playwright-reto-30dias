4ef7e632f1089ee7585919fc0ae068ea3a03b05c
ssaldarriaga <sebastian_saldarriaga82131@elpoli.edu.co>
2026-06-08 21:22:28 -0500

    se agrega login saucedemo

---
+++ tests/training.spec.ts
@@ -0,0 +1,11 @@
+import {test} from '@playwright/test'
+
+// page = fixture
+
+test('"test login saucedemo"', async({page}) => {
+    
+await page.goto('https://www.saucedemo.com/')
+await page.getByRole('textbox', {name: 'Username'}).fill('standard_user')
+await page.getByRole('textbox', {name: 'Password'}).fill('secret_sauce')
+await page.getByRole('button', {name: 'Login'}).click()
+})
