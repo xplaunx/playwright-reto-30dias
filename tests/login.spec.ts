41a22d0926c72d9b2edab3bb9126688b4faeb209
ssaldarriaga <sebastian_saldarriaga82131@elpoli.edu.co>
2026-06-09 20:55:11 -0500

    Se agrega escenarios de fallo ejercicio 1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,3 +10,28 @@ test('login hrm', async({page}) => {
     await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
 
 })
+
+
+test('login incorrect user hrm', async({page}) => {
+
+await page.goto('https://opensource-demo.orangehrmlive.com/')
+await page.getByRole('textbox', {name: 'Username'}).fill('UsuarioNoExiste')
+await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
+await page.getByRole('button', {name: 'Login'}).click()
+
+await expect(page.getByText('Invalid credentials')).toBeVisible()
+
+})
+
+
+test('login hrm incorrect password', async({page}) => {
+
+await page.goto('https://opensource-demo.orangehrmlive.com/')
+await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
+await page.getByRole('textbox', {name: 'Password'}).fill('Incorrecta')
+await page.getByRole('button', {name: 'Login'}).click()
+
+await expect(page.getByText('Invalid credentials')).toBeVisible()
+
+
+})
