58c71c455077d9519a8a5d64abfeaf431bf0511d
ssaldarriaga <sebastian_saldarriaga82131@elpoli.edu.co>
2026-06-13 16:10:06 -0500

    reto 4

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -41,6 +41,33 @@ test('Check left menu options', async ({ page }) => {
     ];
 
     expect(currentMenuItems).toEqual(expectedMenuItems)
-    expect(currentMenuItems[0]).toEqual('Admin')
+    expect(currentMenuItems[0]).toEqual(expectedMenuItems[0])
+
+})
+
+test('Navigate throught the left panel', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemsCount = await leftMenuItems.count()
+
+    for (let i = 0; i < currentMenuItemsCount; i++) {
+        const menuItem = leftMenuItems.nth(i)
+        const menuText = await menuItem.innerText()
+
+        console.log('Current menu item', menuText)
+        await menuItem.click()
+
+        if (menuText == 'Maintenance') {
+            await page.goBack();
+        }
+    }
+
 
 })
